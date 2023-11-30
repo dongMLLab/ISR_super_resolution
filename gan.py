f848821e60d683f7f1f7b1e7b2b5a963b004e516
donghquinn <ehdgus1524@gmail.com>
2023-11-30 23:23:34 +0900

    wip

--- gan.py
+++ gan.py
@@ -6,6 +6,9 @@ import os
 image_dir = os.listdir("./img")
 
 for i in image_dir:
+    if i[:-2] == "md":
+        print("Pass Not Image file: {}".format(i[:-2]))
+        pass
 # /00037-3574770352.jpg
     img = Image.open('./img/'+i)
 
@@ -16,5 +19,5 @@ for i in image_dir:
     sr_img = rrdn.predict(lr_img)
     image = Image.fromarray(sr_img)
 
-    image.save("results/"+i)
+    image.save("results/gan/"+"gans_"+i)
     print("Resolution Finished: {}".format(i))
--- main.py
+++ main.py
@@ -7,14 +7,20 @@ image_dir = os.listdir("./img")
 
 for i in image_dir:
 # /00037-3574770352.jpg
+    if i[:-2] == "md":
+        print("Pass Not Image file: {}".format(i[:-2]))
+        pass
     img = Image.open('./img/'+i)
 
     lr_img = np.array(img)
 
-    rdn = RDN(weights='psnr-small')
+    # psnr-large, psnr-small, noise-cancel
+
+    weights = "psnr-large"
+    rdn = RDN(weights=weights)
 
     sr_img = rdn.predict(lr_img)
     image = Image.fromarray(sr_img)
 
-    image.save("results/"+i)
+    image.save("results/isr/" + weights +"_" + i)
     print("Resolution Finished: {}".format(i))
