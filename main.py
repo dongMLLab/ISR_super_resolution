6b7f4eba0d3f5acbc7dba62b64f2bcbad8d44af7
Kim Donghyun <kimdonghyun@Kims-MacBook-Air.local>
2023-12-01 15:07:11 +0900

    fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dto.upload import UploadRequestDto
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from minio import S3Error
-
+import os
 from lib.run import run_main
 
 warnings.filterwarnings(action='ignore')
@@ -18,7 +18,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-client = MinioClient()
+minio_endpoint = os.getenv("MINIO_ENDPOINT")
+minio_access = os.getenv("MINIO_ACCESSKEY")
+minio_secret = os.getenv("MINIO_SECRET")
+
+client = MinioClient(
+    minio_endpoint,
+    minio_access,
+    minio_secret
+)
 
 @app.router.post("/resolution")
 def super_resolution(request: UploadRequestDto):
